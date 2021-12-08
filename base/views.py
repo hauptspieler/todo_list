eb06d9daa91ca3bcd380fbbeea5b3e2e14cf8c90
hauptspieler <baisch474@gmail.com>
2021-12-08 20:58:14 -0300

    Full Create, Update and Delete

--- base/views.py
+++ base/views.py
@@ -1,8 +1,11 @@
+from django.db.models import fields
 from django.http.response import HttpResponseGone
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from base.models import Task
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
 
 class TaskList(ListView):
@@ -15,3 +18,21 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     # template_name = 'base/task.html   para mudar o nome do template HTML
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    context_object_name = 'task'
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-50smd(gyfa_0^)@otz-5quf+=20$9*xfwid^+s7k7kh-2sdivn'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'  # 1 == True
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
