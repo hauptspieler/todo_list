3a5588dfa4b712bff7cc0fdf0a68e2dd7f84625f
hauptspieler <baisch474@gmail.com>
2021-12-08 21:27:40 -0300

    .env config on settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -19,12 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-<<<<<<< HEAD
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
     'SECRET_KEY')
-=======
->>>>>>> eb203fdb9b7b32e7e8f1981e0311293cec630487
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'  # 1 == True
